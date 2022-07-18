24a79ec0910171d44144a311cc23c8398c451a45
Paul van de Ven <paul@gutstickets.org>
2022-07-18 19:43:42 +0200

    lsp module on_attach

--- nvim/.config/nvim/lua/_plugins.lua
+++ nvim/.config/nvim/lua/_plugins.lua
@@ -97,9 +97,6 @@ return packer.startup(function()
   use({
     "kyazdani42/nvim-tree.lua",
     requires = { "kyazdani42/nvim-web-devicons" },
-    config = function()
-      require("nvim-tree").setup({})
-    end,
   })
   use({
     "rrethy/vim-hexokinase",
---
+++ nvim/.config/nvim/lua/lsp/on_attach.lua
@@ -0,0 +1,51 @@
+local api = vim.api
+local diagnostic = vim.diagnostic
+local lsp = vim.lsp
+local lsp_format_augroup = api.nvim_create_augroup("LspFormatting", {})
+
+local utils_ok, utils = pcall(require, "utils")
+if not utils_ok then
+  return
+end
+
+
+return function(client, bufnr)
+  diagnostic.config {
+    underline = true,
+    float = {
+      scope = "cursor",
+      source = "always"
+    },
+    signs = true,
+    update_in_insert = true
+  }
+
+  -- utils.bufmap("n", "ga", "lua vim.lsp.buf.code_action()")
+  -- utils.bufmap("n", "gd", "lua vim.lsp.buf.definition()")
+  -- utils.bufmap("n", "gr", "lua vim.lsp.buf.references()")
+  -- utils.bufmap("n", "K", "lua vim.lsp.buf.hover()")
+  -- utils.bufmap("n", "gl", "lua vim.lsp.diagnostic.show_line_diagnostics()")
+
+  api.nvim_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+
+  -- autofmt on save
+  if client.supports_method("textDocument/formatting") then
+    api.nvim_clear_autocmds({
+      group = lsp_format_augroup,
+      buffer = bufnr
+    })
+  end
+
+  api.nvim_create_autocmd("BufWritePre", {
+    group = lsp_format_augroup,
+    buffer = bufnr,
+    callback = function()
+      lsp.buf.formatting_sync({
+        filter = function(c)
+          return c.name == "null-ls"
+        end,
+      })
+    end
+  })
+
+end
